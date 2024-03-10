768ad75a55df07504e3e9a54ad582cd420c6587f
ahmad027 <mutaahmad10@gmail.com>
2024-03-10 23:53:17 +0700

    belajar pengulangan WHILE, FOR dan pengulangan IF ELSE tgl 10 Maret 2024

---
+++ LATIHAN2/012- Latihan Pengulangan While/script.js
@@ -0,0 +1,25 @@
+/* percobaan pertama, dihentikan oleh user */
+
+// let ulang = true;
+
+// while (ulang) {
+//   console.log('Hello world!');
+//   ulang = confirm('lagi?');
+// }
+
+/* percobaan kedua, dihentikan oleh program  */
+
+// let nilaiAwal = 1;
+// while (nilaiAwal <= 5) {
+//   console.log('Hello World!!!');
+//   nilaiAwal++;
+// }
+
+/* percobaan ketiga, membuat tulisan urut dari 1 - costuma ngka yng diinginkan   */
+
+let nilaiAwal = 1;
+
+while (nilaiAwal <= 10) {
+  console.log('Hello World ' + nilaiAwal + ' X');
+  nilaiAwal++;
+}
---
+++ LATIHAN2/012-latihan tugas angkot/script.js
@@ -0,0 +1,6 @@
+let jumlahAngkot = 10;
+let noAngkot = 1;
+while (noAngkot <= jumlahAngkot) {
+  console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik');
+  noAngkot++;
+}
