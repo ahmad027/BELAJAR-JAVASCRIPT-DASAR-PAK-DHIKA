8c5fefb0261b80d05e52c417976187143f2c8cb8
ahmad027 <mutaahmad10@gmail.com>
2024-04-17 12:43:12 +0700

    latihan pengulangan dan pengkondisian bersarang tgl 17 April 2024

--- LATIHAN2/020- Latihan pengulangan dan pengkondisian bersarang/script.js
+++ LATIHAN2/020- Latihan pengulangan dan pengkondisian bersarang/script.js
@@ -1,6 +1,8 @@
+// bintang beruutan kecil ke besar
+
 let s = '';
 
-for (let i = 10; i > 0; i--) {
+for (let i = 0; i <= 10; i++) {
   for (let j = 0; j < i; j++) {
     s += '*';
   }
@@ -8,3 +10,17 @@ for (let i = 10; i > 0; i--) {
 }
 
 console.log(s);
+console.log('\n');
+
+// bintang berurutan besar ke kecil
+
+let t = '';
+
+for (let k = 5; k > 0; k--) {
+  for (let l = 0; l < k; l++) {
+    t += '*';
+  }
+  t += '\n';
+}
+
+console.log(t);
